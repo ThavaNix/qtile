21a6297949985f0b76dddd70099deb972a350db3
ThavaNix <thavaprasanthnavaraththinam@gmail.com>
2019-07-04 09:42:14 +0200

    Try

--- git.sh
+++ git.sh
@@ -1,3 +1,9 @@
+if git remote != origin ; then
+  echo "Enter Your Repo Name"
+  read repo
+  git  remote add origin https://github.com/thavanix/$repo
+fi
+
 git remote -v
 git add --all .
 
@@ -5,5 +11,5 @@ echo "##################################################################"
 echo "################     Enter Commit Tag           ##################"
 echo "##################################################################"
 read input
-git commit -m '$input'
+git commit -m $input
 git push -u origin master
